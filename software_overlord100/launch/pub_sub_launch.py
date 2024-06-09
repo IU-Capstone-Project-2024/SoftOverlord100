599a3c90ba84fca50412253c4d241ada5760d711
Ivan Domrachev <domrachev03@mail.ru>
2024-06-09 20:58:27 +0000

    ci: python and cpp autoformatting

--- software_overlord100/launch/pub_sub_launch.py
+++ software_overlord100/launch/pub_sub_launch.py
@@ -1,16 +1,12 @@
 from launch import LaunchDescription
 from launch_ros.actions import Node
 
+
 def generate_launch_description():
-    return LaunchDescription([
-        Node(
-            package='software_overlord100',
-            executable='publisher',
-            name='pub'
-        ),
-        Node(
-            package='software_overlord100',
-            executable='subscriber',
-            name='sub'
-        ),
-    ])
+
+    return LaunchDescription(
+        [
+            Node(package="software_overlord100", executable="publisher", name="pub"),
+            Node(package="software_overlord100", executable="subscriber", name="sub"),
+        ]
+    )
