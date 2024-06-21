d0d01b338cebe3e60ddb434d4c4c5295e5e588c7
Ivan Domrachev <domrachev03@mail.ru>
2024-06-21 14:17:05 +0000

    refactor: remove redundant resource

--- urdf_dummy/setup.py
+++ urdf_dummy/setup.py
@@ -10,11 +10,10 @@ setup(
     version="0.0.0",
     packages=find_packages(exclude=["test"]),
     data_files=[
-        ("share/ament_index/resource_index/packages", ["resource/" + package_name]),
         ("share/" + package_name, ["package.xml"]),
         (
             os.path.join("share", package_name, "launch"),
-            glob(os.path.join("launch", "*launch.[pxy][yma]*")),
+            glob(os.path.join("launch", "*")),
         ),
         (
             os.path.join("share", package_name, "description"),
