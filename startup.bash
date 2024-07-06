591bfa8a69c21c29470863bb038f1fdaefa42f37
Veronika Levasheva <v.levasheva@innopolis.university>
2024-07-06 15:46:06 +0000

    Update startup.bash

--- startup.bash
+++ startup.bash
@@ -3,5 +3,5 @@
 # Note: effect of "export DISPLAY=host.docker.internal:0.0" on Linux systems is unknown 
 colcon build && 
 source install/setup.bash &&
-export DISPLAY=host.docker.internal:0.0 &&  
+#export DISPLAY=host.docker.internal:0.0 &&  
 ros2 launch urdf_dummy sim.launch.py
