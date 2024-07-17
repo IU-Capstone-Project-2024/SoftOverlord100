e6f5d2f73f6619c01d847fab8c4ffc7cdf519d00
ekaterina <mozhegova04@gmail.com>
2024-07-17 16:13:38 +0300

    Manual mode with slam is ready for the hardware. v1.0

--- overlord100_bringup/launch/manual_control.launch.py
+++ overlord100_bringup/launch/manual_control.launch.py
@@ -7,11 +7,6 @@ from launch_ros.actions import Node
 def generate_launch_description():
     return LaunchDescription(
         [
-            # Launch URDF simulation
-            ExecuteProcess(
-                cmd=["ros2", "launch", "urdf_dummy", "sim_custom_controller.launch.py"],
-                output="screen",
-            ),
             # Start diff_drive_controller node
             Node(
                 package="overlord100_controller",
